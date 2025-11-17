3144c359817b5350e51eb0201260ae11cad675f0
WPJ-Dev <wpruittDev@gmail.com>
2025-11-17 16:33:37 -0600

    adding post functionality

--- routes/ideas.js
+++ routes/ideas.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
     res.send({ success: true, data: ideas });
 });
 
+// Get idea by id
 router.get('/:id', (req, res) => {
     const idea = ideas.find((idea) => idea.id === +req.params.id)
 
@@ -17,5 +18,20 @@ router.get('/:id', (req, res) => {
     res.send({ success: true, data: idea });
     }
 });
+
+// Add an idea
+router.post('/', (req, res) => {
+    const idea = {
+        id: ideas.length + 1,
+        text: req.body.text,
+        tag: req.body.tag,
+        username: req.body.username,
+        date: new Date().toISOString().slice(0, 10),
+    };
+
+    ideas.push(idea);
+
+    res.json({ success: true, data: idea })
+})
 
 module.exports = router;
