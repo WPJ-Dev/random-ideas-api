8973d354ba50ccdf160eec578af40d1a8a0dc391
WPJ-Dev <wpruittDev@gmail.com>
2025-11-18 10:13:02 -0600

    added basic put/delete functionality

--- routes/ideas.js
+++ routes/ideas.js
@@ -15,7 +15,7 @@ router.get('/:id', (req, res) => {
     if(!idea) {
         res.status(404).json({ success: false, error: 'Resource not found' })
     } else {
-    res.send({ success: true, data: idea });
+        res.send({ success: true, data: idea });
     }
 });
 
@@ -32,6 +32,36 @@ router.post('/', (req, res) => {
     ideas.push(idea);
 
     res.json({ success: true, data: idea })
-})
+});
+
+// Put idea by id
+router.put('/:id', (req, res) => {
+    const idea = ideas.find((idea) => idea.id === +req.params.id)
+
+    if(!idea) {
+        res.status(404).json({ success: false, error: 'Resource not found' })
+    } else {
+        idea.text = req.body.text || idea.text;
+        idea.tag = req.body.tag || idea.tag;
+
+        res.json({ success: true, data: idea })
+    }
+
+
+});
+
+// Delete idea by id
+router.delete('/:id', (req, res) => {
+    const idea = ideas.findIndex((idea) => idea.id === +req.params.id)
+
+    if(idea === -1) {
+        res.status(404).json({ success: false, error: 'Resource not found' })
+    } else {
+        ideas.splice(idea, 1);
+    
+        res.json({ success: true});
+    }
+    
+});
 
 module.exports = router;
